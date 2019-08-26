a91f0e1eadddddbbd5944ad4a0b255fef19ef70f
Laurens Verschuere <laurens@massivemedia.eu>
2019-08-27 00:46:02 +0200

    fix generate config first

--- sbfspot/run.sh
+++ sbfspot/run.sh
@@ -7,15 +7,13 @@ rc-status -a
 echo 'Show all services.'
 rc-status -s
 
-rc-service SBFspotUploadDaemon.service start
-
-# --- CREATE CRON ----
-
-
 # --- GENERATE CONFIG --
 ./generateConfig.sh SBFspot.cfg SBFspotUpload.cfg
 
 # ---- RUN ----
+# start service
+rc-service SBFspotUploadDaemon.service start
+
 # cron
 echo 'Starting cron in foreground'
 /usr/sbin/crond -f
