f5a916966eca254588336735ae3b5268dd4c1be0
Laurens Verschuere <laurens@massivemedia.eu>
2019-12-15 13:49:31 +0100

    Fix + bump

--- surveillancestream/run.sh
+++ surveillancestream/run.sh
@@ -1,15 +1,30 @@
 # config
+cat /data/options.json
 CONFIG_PATH=/data/options.json
 CONFIG_DB_URL="$(jq --raw-output '.db_url' $CONFIG_PATH)"
 CONFIG_RECORDING_FOLDER="$(jq --raw-output '.recording_folder' $CONFIG_PATH)"
 
+echo "CONFIG_DB_URL=${CONFIG_DB_URL}"
+echo "CONFIG_RECORDING_FOLDER=${CONFIG_RECORDING_FOLDER}"
+
+ls -lah /opt/surveillancestream/
+
 echo "db_url=${CONFIG_DB_URL}" > /opt/surveillancestream/backend/.env
-echo "recording_folder=${CONFIG_RECORDING_FOLDER}" >> /opt/surveillancestream/backend/.env \
-echo "REACT_APP_GQL_HOST=http://localhost:4000" > /opt/surveillancestream/frontend/.env \
+echo "recording_folder=${CONFIG_RECORDING_FOLDER}" >> /opt/surveillancestream/backend/.env
+cat /opt/surveillancestream/backend/.env
+
+echo "REACT_APP_GQL_HOST=http://localhost:4000" > /opt/surveillancestream/frontend/.env
 echo "REACT_APP_STATIC_HOST=http://localhost:3001" >> /opt/surveillancestream/frontend/.env
+cat /opt/surveillancestream/frontend/.env
 
-# build
-cd backend && yarn run build && cd .. && cd frontent && yarn run build && cd ..
+# build backend
+echo 'Build backend'
+cd /opt/surveillancestream/backend && yarn run build
 
+#build frontend
+echo 'Build frontent'
+cd /opt/surveillancestream/frontent && yarn run build && cd ..
+
+echo 'Run'
 # run
-node dist/index.js
+node /opt/surveillancestream/dist/index.js
