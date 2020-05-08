05470054a9796edbfefce7025b4f3effdcb4f68e
Laurens Verschuere <laurens@massivemedia.eu>
2020-05-08 12:04:42 +0200

    Update create .env files

--- surveillancestream/run.sh
+++ surveillancestream/run.sh
@@ -12,8 +12,10 @@ CONFIG_RECORDING_FOLDER="$(jq --raw-output '.recording_folder' $CONFIG_PATH)"
 # ls -lah /opt/surveillancestream/
 
 # create .env files
-echo "db_url=${CONFIG_DB_URL}" > /opt/surveillancestream/backend/.env
-echo "recording_folder=${CONFIG_RECORDING_FOLDER}" >> /opt/surveillancestream/backend/.env
+echo "db_url=${CONFIG_DB_URL}" > /opt/surveillancestream/backend/backend/prisma/.env
+
+echo "recording_folder=${CONFIG_RECORDING_FOLDER}" > /opt/surveillancestream/backend/.env
+# cat /opt/surveillancestream/backend/backend/prisma/.env
 # cat /opt/surveillancestream/backend/.env
 
 echo "REACT_APP_GQL_HOST=http://localhost:4000" > /opt/surveillancestream/frontend/.env
@@ -24,7 +26,7 @@ echo "REACT_APP_STATIC_HOST=http://localhost:3001" >> /opt/surveillancestream/fr
 echo 'Build backend'
 cd /opt/surveillancestream/backend && yarn run build
 
-#build frontend
+# build frontend
 echo 'Build frontent'
 cd /opt/surveillancestream/frontend && yarn run build && cd ..
 
